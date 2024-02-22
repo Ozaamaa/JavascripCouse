29806dfd0e1deffb1c467a38df6ce5fdecc82ec3
Attary <Ozaamaa>
2024-02-22 14:16:56 +0100

    first commit

---
+++ TestJS/test.js
@@ -0,0 +1,34 @@
+
+/**------------ Premier Exercice Variables -------------
+let totalLivres = 500
+totalLivres += 50
+totalLivres -= 10
+totalLivres += 5
+        
+console.log(totalLivres)
+
+let affichage = "Notre Bibliotheque possède "
+affichage += totalLivres
+affichage += " livres"
+ document.write(affichage)
+//document.getElementById("sum").innerHTML=affichage
+
+**/
+/**------ Exo 2 Creation Objet ---------- 
+let ticket = {
+    nomFilm: "Batman",
+    prix: 10,
+    numeroSalle: 4
+}
+
+let nom = "Mocro"
+
+let affichageTicket = "Bonjour " +nom+ ", votre Film " +ticket.nomFilm+ " est en salle " +ticket.numeroSalle
+document.write(affichageTicket)
+
+**/
+
+/** -------------Exo 3 Les Tableaus --------- */
+
+
+
